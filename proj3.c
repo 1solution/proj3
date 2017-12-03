326517367be5330947469748a6e77958664318c5
Mikee <Michal.Plsek@seznam.cz>
2017-12-03 19:19:41 +0100

    Update proj3.c

--- proj3.c
+++ proj3.c
@@ -46,29 +46,30 @@ struct cluster_t { // CLUSTER X OBJEKTU:
     struct obj_t *obj; // ukazatel na vlastni objekt
 };
 
-/*
- Inicializace shluku 'c'. Alokuje pamet pro cap objektu (kapacitu).
- Ukazatel NULL u pole objektu znamena kapacitu 0.
-*/
 void init_cluster(struct cluster_t *c, int cap) {
     assert(c != NULL);
     assert(cap >= 0);
-    c->size = 0;
-    c->obj = malloc(cap * sizeof(struct obj_t));
-}
 
+    c->size = 0;
+    c->capacity = cap;
+    c->obj = NULL; // NULL is result in c->obj if c->cap is zero
+
+    if(cap > 0) {
+        c->obj = malloc(cap * sizeof(struct obj_t));
+
+    if(c->obj == NULL)
+        c->capacity = 0;
+    }
+}
 /*
- Odstraneni vsech objektu shluku a inicializace na prazdny shluk.
- */
 void clear_cluster(struct cluster_t *c) {
   free(c->obj);
   init_cluster(c,0);
 }
+*/
 
 const int CLUSTER_CHUNK = 10; // when reallocationg, use this value as step+
-/*
- Zmena kapacity shluku 'c' na kapacitu 'new_cap'.
- */
+
 struct cluster_t *resize_cluster(struct cluster_t *c, int new_cap) {
     assert(c);
     assert(c->capacity >= 0);
@@ -87,52 +88,65 @@ struct cluster_t *resize_cluster(struct cluster_t *c, int new_cap) {
     c->capacity = new_cap;
     return c;
 }
-
+/*
 void append_cluster(struct cluster_t *c, struct obj_t obj) {
   if(c->size >= c->capacity)
     resize_cluster(c,CLUSTER_CHUNK+c->capacity);
      c->obj[c->size] = obj; // merge new obj
     c->size += 1;
-}
+}*/
 
 int load_clusters(char *filename, struct cluster_t **arr) {
 	assert(arr != NULL);
 
 	FILE *foo;
-  foo = fopen(filename,"r");
+    foo = fopen(filename,"r");
 
-  if(foo == NULL)
-    printf("File does not exist.\n");
-
-  int c = 0, i = 0;
-  char fraze[] = "count=";
-
-  while(c != EOF || c!= '\n') {
-  if(i == 6) {
-    char number[100] = {'\0'};
-    int j = 0;
-    while(c != EOF || c != '\n') {
-      if(isdigit(c)) {
-        number[j] = (c = fgetc(foo));
-      }
-      else {
-        // error
-      }
+    if(foo == NULL) {
+        *arr = NULL;
+        fprintf(stderr,"%s","File does not exist.\n");
+        return 0;
     }
 
-    char *end;
-    int newInt = strtol(number, &end);
-    printf("%d", newInt);
-  }
-    c = fgetc(foo);
-  if(c == fraze[i] && i < 6)
-    i++;
-  }
+    char fraze[] = "count=";// [0 1 2 3 4 5]
+    int c = 0;
+    int i = 0;
+    int nStr[6] = {'\0'};
+    int n = 0;
+    while(c != EOF || c!= '\n') {
 
-  int n = 0;
+        c = fgetc(foo);
+
+        if(c == fraze[i] && i < 6)
+            i++;
+
+        if(i == 6) {
+                c = fgetc(foo);
+                    int j = 0;
+                while(1) {
+                    if(isdigit(c)) {
+                        nStr[j] = c - 48;
+                        j++;
+                    }
+                    else if((c == '\n' || c == '\r') && j > 0) {
+
+                        for(int i = 0; i < j; i++)
+                            n = n + (nStr[j-i-1] * pow(10,i));
+                        return n;
+                    }
+                    else {
+                        fprintf(stderr,"%s","Wrong input file format - N is not a number or missing.\n");
+                        return 0;
+                    }
+                    c = fgetc(foo);
+                }
+
+        }
+
+    }
 
   struct cluster_t *clusters = malloc(n*sizeof(struct cluster_t));
-  *arr = clusters; // uloz ukazatel na prvni shluk v poli *arr[] - **arr
+  *arr = clusters;
 
   int id = 0;
   float x = 0.0;
@@ -140,14 +154,14 @@ int load_clusters(char *filename, struct cluster_t **arr) {
   char endline;
 
   for(int i = 0; i < n; i++) {
-    fscanf(foo,"%d %f %f%c",&id,&x,&y,&endline);
+    fscanf(foo,"%i%f%f%c",&id,&x,&y,&endline);
 
-    if(endline == EOF || endline == '\n') {
+    /*if(endline == EOF || endline == '\n') {*/
       struct cluster_t soloCluster;
       init_cluster(&soloCluster,1);
       soloCluster.size = 1;
 
-      if(x >= 0 || x <= 1000 || y >= 0 || y <= 1000) {
+      if(x >= 0 && x <= 1000 && y >= 0 && y <= 1000) {
         clusters[i] = soloCluster;
         clusters[i].obj->id = id;
         clusters[i].obj->x = x;
@@ -156,37 +170,36 @@ int load_clusters(char *filename, struct cluster_t **arr) {
       else {
         *arr = NULL;
         fprintf(stderr,"%s","X or Y from file is not in correct format.\n");
-        break;
+        return 0;
       }
-    }
-    else {
+
+    /*}*/
+    /*else {
       *arr = NULL;
       fprintf(stderr,"%s","Something wrong in input file.\n");
       break;
-    }
+    }*/
   }
   fclose(foo);
 	return n;
 }
 
-/*
- Seradi objekty ve shluku 'c' vzestupne podle jejich identifikacniho cisla.
- */
-void sort_cluster(struct cluster_t *c);
+/*void sort_cluster(struct cluster_t *c);
 
-/*
- Do shluku 'c1' prida objekty 'c2'. Shluk 'c1' bude v pripade nutnosti rozsiren.
- Objekty ve shluku 'c1' budou serazeny vzestupne podle identifikacniho cisla.
- Shluk 'c2' bude nezmenen.
- */
 void merge_clusters(struct cluster_t *c1, struct cluster_t *c2)
 {
     assert(c1 != NULL);
     assert(c2 != NULL);
 
-    // TODO
-}
+    if(c1->capacity < c1->size+c2->size)
+        resize_cluster(c1, c1->size+c2->size);
 
+    for(int i = 0; i < c2->size; i++)
+        append_cluster(c1, c2->obj[i]);
+
+    sort_cluster(c1);
+}
+*/
 /* Prace s polem shluku */
 
 /*
@@ -194,17 +207,20 @@ void merge_clusters(struct cluster_t *c1, struct cluster_t *c2)
  (shluku). Shluk pro odstraneni se nachazi na indexu 'idx'. Funkce vraci novy
  pocet shluku v poli.
 */
-int remove_cluster(struct cluster_t *carr, int narr, int idx)
+/* int remove_cluster(struct cluster_t *carr, int narr, int idx)
 {
     assert(idx < narr);
     assert(narr > 0);
 
-    // TODO
-}
+    clear_cluster(&carr[idx]);
+    carr->size -= 1;
 
+    return carr->size;
+}
+*/
 /*
  Pocita Euklidovskou vzdalenost mezi dvema objekty.
- */
+ *//*
 float obj_distance(struct obj_t *o1, struct obj_t *o2)
 {
     assert(o1 != NULL);
@@ -212,10 +228,10 @@ float obj_distance(struct obj_t *o1, struct obj_t *o2)
 
     // TODO
 }
-
+*/
 /*
  Pocita vzdalenost dvou shluku.
-*/
+
 float cluster_distance(struct cluster_t *c1, struct cluster_t *c2)
 {
     assert(c1 != NULL);
@@ -224,14 +240,14 @@ float cluster_distance(struct cluster_t *c1, struct cluster_t *c2)
     assert(c2->size > 0);
 
     // TODO
-}
+}*/
 
 /*
  Funkce najde dva nejblizsi shluky. V poli shluku 'carr' o velikosti 'narr'
  hleda dva nejblizsi shluky. Nalezene shluky identifikuje jejich indexy v poli
  'carr'. Funkce nalezene shluky (indexy do pole 'carr') uklada do pameti na
  adresu 'c1' resp. 'c2'.
-*/
+*//*
 void find_neighbours(struct cluster_t *carr, int narr, int *c1, int *c2)
 {
     assert(narr > 0);
@@ -239,7 +255,6 @@ void find_neighbours(struct cluster_t *carr, int narr, int *c1, int *c2)
     // TODO
 }
 
-// pomocna funkce pro razeni shluku
 static int obj_sort_compar(const void *a, const void *b) {
     const struct obj_t *o1 = (const struct obj_t *)a;
     const struct obj_t *o2 = (const struct obj_t *)b;
@@ -248,17 +263,12 @@ static int obj_sort_compar(const void *a, const void *b) {
     return 0;
 }
 
-/*
- Razeni objektu ve shluku vzestupne podle jejich identifikatoru.
-*/
 void sort_cluster(struct cluster_t *c) {
     qsort(c->obj, c->size, sizeof(struct obj_t), &obj_sort_compar);
-}
+}*/
 
-/*
- Tisk shluku 'c' na stdout.
-*/
 void print_cluster(struct cluster_t *c) {
+
     for (int i = 0; i < c->size; i++)
     {
         if (i) putchar(' ');
@@ -267,10 +277,6 @@ void print_cluster(struct cluster_t *c) {
     putchar('\n');
 }
 
-/*
- Tisk pole shluku. Parametr 'carr' je ukazatel na prvni polozku (shluk).
- Tiskne se prvnich 'narr' shluku.
-*/
 void print_clusters(struct cluster_t *carr, int narr)
 {
     printf("Clusters:\n");
@@ -285,10 +291,7 @@ int main(int argc, char *argv[])
 {
     struct cluster_t *clusters;
 
-    load_clusters("C:\\Users\\xplsek03\\Desktop\\proj3\\bin\\Debug\\objekty",&clusters);
-
-    print_clusters(clusters, 5);
-
+    print_clusters(clusters, load_clusters("/home/mikee/Desktop/proj3/bin/Debug/objekty",&clusters));
 
     return 0;
 
